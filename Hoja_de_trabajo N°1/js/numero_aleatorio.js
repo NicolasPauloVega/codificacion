2b0ee1714b0dcbe70f24bdacdefa61531f407bcf
NicolasPV <145580278+NicolasPauloVega@users.noreply.github.com>
2024-04-18 10:24:32 -0500

    18-04-2024

--- Hoja_de_trabajo N°1/js/numero_aleatorio.js
+++ Hoja_de_trabajo N°1/js/numero_aleatorio.js
@@ -1,3 +1,15 @@
-function aleatorio() {
-    
+function aleatorio(min, max) {
+    //Establecemos los valores minimos y maximos.
+    min = 1;
+    max = 100;
+
+    //Creamos una variable que almacene los valores de valor numerico que venga del input
+    var numero = parseInt( document.getElementById("nro1").value );
+    var array = []; //Creamos un arreglo que almacena los numeros aleatorios.
+
+    //Creamos un siclo que se ejecute siempre y cuando la longitud del array sea menor que el numero colocado por el usuario.
+    while (array.length < numero) {
+        array.push(Math.floor(101*Math.random()));//Genera numeros enteros (Math.floor redondea al numero entero mas sercano) aleatorios entre el 0 y el 100
+        document.getElementById("resultado").innerHTML="El resultado de los numeros escogidos de manera aleatoria son: " + array; //Mostramos el resultado deseado en pantalla.
+    }
 }
