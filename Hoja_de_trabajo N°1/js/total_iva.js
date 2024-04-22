10314b1ecb107e67155ec91665171ef2ffafe12e
NicolasPV <145580278+NicolasPauloVega@users.noreply.github.com>
2024-04-22 07:03:29 -0500

    22-04-2024

--- Hoja_de_trabajo N°1/js/total_iva.js
+++ Hoja_de_trabajo N°1/js/total_iva.js
@@ -1,5 +1,5 @@
 function calcular(nro1, nro2) {
-    nro1 = parseFloat( document.getElementById("nro1").value );
+    nro1 = parseInt( document.getElementById("nro1").value );
     nro2 = parseFloat( document.getElementById("nro2").value );
 
     var iva = nro2 / 100;
@@ -10,5 +10,5 @@ function calcular(nro1, nro2) {
         var calculo = nro1 * 0.19;
     }
 
-    document.getElementById("total").innerHTML = "El total por el producto es de $" + calculo;
+    document.getElementById("total").innerHTML = "El total por el producto es de $" + calculo.toFixed(2);
 }
