3fee476b1ffcaed0b6cb5af5ab83db6195505dfe
NicolasPV <145580278+NicolasPauloVega@users.noreply.github.com>
2024-04-24 10:53:10 -0500

    24-04-2024

---
+++ Hoja_de_trabajo N°4/js/app.js
@@ -0,0 +1,18 @@
+const TaskManager = {
+    tasks: [],
+    
+    addTask(Tarea) {
+        task = document.getElementById("enter-task").value;
+
+        const Tarea = {
+            name: Tarea,
+            complete: false
+        }
+
+        Tarea.push(tasks);
+    },
+
+    render(){
+        
+    }
+}
