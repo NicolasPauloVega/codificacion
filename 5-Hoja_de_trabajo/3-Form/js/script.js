150cad87bcd508b271f8c1e25bab522955167c73
NicolasPV <145580278+NicolasPauloVega@users.noreply.github.com>
2024-05-06 09:28:22 -0500

    06-05-2024

---
+++ 5-Hoja_de_trabajo/3-Form/js/script.js
@@ -0,0 +1,5 @@
+// Guardamos una referencia de cada elemento del formulario
+const form = document.getElementById('form');
+const username = document.getElementById('username');
+const email = document.getElementById('email');
+const password = document.getElementById('password');
